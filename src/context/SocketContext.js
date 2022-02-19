9d0b633d1f930f49983fde6d10b1f7b42e14ba9f
LUCAS ARIEL SOUZA PINO <lucas.pino@wds.local>
2022-02-19 11:14:32 -0300

    refactor(socket, signin, build): arrumando build

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import socketio from 'socket.io-client';
 import PropTypes from 'prop-types';
+import { enviroment } from 'config/axios';
+const { REACT_APP_API_URL } = process.env;
 
-const socket = socketio.connect('http://localhost:3333');
+const socket = socketio.connect(REACT_APP_API_URL ? REACT_APP_API_URL : enviroment.api_production);
 const SocketContext = React.createContext();
 
 const SocketProvider = ({ children }) => {
