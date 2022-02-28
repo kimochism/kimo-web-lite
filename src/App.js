0c3ef43989bd6ed1cb216d8c70d286405d823045
Lucas Mandala <lucas@mandalaclothing.com.br>
2022-02-28 11:35:50 -0300

    fix(effect scroll): remove

--- src/App.js
+++ src/App.js
@@ -7,28 +7,24 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // facebook chat
-// import MessengerCustomerChat from 'react-messenger-customer-chat';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 // contexts
 import { AuthProvider } from 'context/AuthContext';
 import { SocketProvider } from 'context/SocketContext';
 
-// import { UseEffectScroll } from 'react-use-smooth-scroll';
-
 const App = () => {
 	return (
 		<AuthProvider>
 			<SocketProvider>
-				{/* <UseEffectScroll> */}
 					<Router>
 						<Routes />
-						{/* <MessengerCustomerChat
+						<MessengerCustomerChat
 							pageId="105137654636679"
 							appId="330530065247617"
-						/> */}
+						/>
 						<ToastContainer autoClose={5000} style={{ padding: '0px', width: '500px' }} />
 					</Router>
-				{/* </UseEffectScroll> */}
 			</SocketProvider>
 		</AuthProvider>
 	);
